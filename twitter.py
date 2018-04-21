9c65f2d6c12ed96d24e979b996e161d9a51f17cf
Nilay Pachauri <pachauri@usc.edu>
2018-04-21 03:09:19 -0700

    Added comments

--- twitter.py
+++ twitter.py
@@ -2,24 +2,38 @@ import json
 import requests
 import tweepy
 
+def convert(temp, unit):
+    unit = unit.lower()
+    if unit == "c":
+        return 9.0 / 5.0 * temp + 32
+    if unit == "f":
+        return (temp - 32)  / 9.0 * 5.0
+
+def get_heat_index(temp, humidity):
+	
+
+# Returns the api object of twitter enabling tweet posting
 def get_api(cfg):
 	auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
 	auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
 	return tweepy.API(auth)
 
+# Extracts a json packet from the webiste and returns a json packet of strings
 def get_cur_loc():
 	send_url = 'https://ipinfo.io'
 	r = requests.get(send_url)
 	return json.loads(r.text, "utf-8")
 
+# Extracts the actual and temperature of a place given the coordinates
 def get_temp_and_humidity(lat, lon):
 	owm = pyowm.OWM('13ff05b52e1127ce27d6c06b1b1cc411')
 	w = owm.weather_at_coords(lat, lon)
 	return [w.get_temperature('celsius'), w.get_humidity()]
 
 
+# Main function
 def main():
-	# Fill in the values noted in previous step here
+	# Store the keys necessary to access the Twitter API
 	cfg = {
 		"consumer_key"			: "lG608JQsOPZrEUuLBGyW6OUv5",
 		"consumer_secret"		: "vDNOXfjkXJ1NDVNMmO603uaMmmrhltj29NiUGBisSfJCScSO75",
@@ -30,13 +44,13 @@ def main():
 	api = get_api(cfg)
 	cur_loc = get_cur_loc()
 	temp_and_humidity = get_temp_and_humidity(float(cur_loc['loc'].split(',')[0]), float(cur_loc['loc'].split(',')[1]))
+	heat_index = get_heat_index(temp_and_humidity)
 	temp_msg = "N/A"
 
 	org = " ".join(cur_loc['org'].split()[1:])
 	city = cur_loc['city']
 	region = cur_loc['region']
 	postal = cur_loc['postal']
-
 
 	tweet = "It seems to be " + temp_msg + " in " + org + ", " + city + ", " + region + " " + postal
 	status = api.update_status(status=tweet)
