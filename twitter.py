35305d4b403abf282dbc246ba817941e8ee1fb51
Nilay Pachauri <pachauri@usc.edu>
2018-04-21 03:30:26 -0700

    Trying to debug return error

--- twitter.py
+++ twitter.py
@@ -22,10 +22,12 @@ def get_heat_index(c_temp, humidity):
 				(5.481717 * 10**-2 * humidity**2 ) + 		\
 				(1.22874 * 10**-3 * f_temp**2 * humidity + 	\
 				(8.5282 * 10**-4 * f_temp * humidity**2 ) - \
-				(1.99 * 10**-6 ** f_temp**2 * humidity**2) 
+				(1.99 * 10**-6 ** f_temp**2 * humidity**2)
 
+	int final_temp = convert(feel_temp, 'F')
+r
 	# Return the converted value of the actual temperature in Celsius
-	return convert(feel_temp, 'F')
+	return final_temp
 
 # Returns the api object of twitter enabling tweet posting
 def get_api(cfg):
