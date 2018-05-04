9070c3ad68b2f1e2f8f87252d1b9d186ff267374
Nilay Pachauri <pachauri@usc.edu>
2018-05-03 19:20:51 -0700

    Fixed blocking statement

--- TempThread.cpp
+++ TempThread.cpp
@@ -78,8 +78,8 @@ void tempThread(void *args)
 					printf("TempThread: received command to publish to topic"
 						   "m3pi-mqtt-example/temp-thread\n");
 					// Block until a appropriate sensor value is given
-					while (sensor.readData() != 0)	
-						sprintf(pub_buf, "%4.2f,%4.2f", sensor.ReadTemperature(CELCIUS), sensor.ReadHumidity());
+					while (sensor.readData() == 0)	{}	
+					sprintf(pub_buf, "%4.2f,%4.2f", sensor.ReadTemperature(CELCIUS), sensor.ReadHumidity());
 					message.qos = MQTT::QOS0;
 					message.retained = false;
 					message.dup = false;
